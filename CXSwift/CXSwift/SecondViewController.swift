5fb95fc807afdada216e3b524101ed3ae61f4081
xiaoma <752787086@qq.com>
2020-05-24 22:28:38 +0800

    First Project

---
+++ CXSwift/CXSwift/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  CXSwift
+//
+//  Created by mahong yang on 2019/6/14.
+//  Copyright © 2019 mahong yang. All rights reserved.
+//
+
+import UIKit
+//import Alamofire
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        let parameters:Dictionary = ["key":"93c921ea8b0348af8e8e7a6a273c41bd"]
+//        AF.request(.GET, "http://apis.haoservice.com/weather/city", parameters: parameters)
+//            .responseJSON { response in
+//
+//                print("result==\(response.result)")   // 返回结果，是否成功
+//                if let jsonValue = response.result.value {
+//                    /*
+//                     error_code = 0
+//                     reason = ""
+//                     result = 数组套字典的城市列表
+//                     */
+//                    print("code: \(jsonValue["error_code"])")
+//                }
+//        }
+//
+//        AF.download(<#T##url: URLConvertible##URLConvertible#>, method: <#T##HTTPMethod#>, parameters: <#T##Encodable?#>, encoder: <#T##ParameterEncoder#>, headers: <#T##HTTPHeaders?#>, interceptor: <#T##RequestInterceptor?#>, to: <#T##DownloadRequest.Destination?##DownloadRequest.Destination?##(URL, HTTPURLResponse) -> (destinationURL: URL, options: DownloadRequest.Options)#>)
+        
+    }
+
+
+}
+
